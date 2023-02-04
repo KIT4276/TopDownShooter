2d10b2ba102cce532bc002f8d2d93cb6ee8267f8
KIT4276 <82262901+KIT4276@users.noreply.github.com>
2023-02-04 18:54:55 +0300

    Done: minFiringRange & maxFiringRange

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -5,28 +5,28 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     private float _distance;
+    private Collider _collider;
 
     [SerializeField]
     private float _speed;
     [SerializeField, Range(1, 250)]
-    private float _firingRange = 2;
+    private float _minFiringRange = 1;
+    [SerializeField, Range(1, 250)]
+    private float _maxFiringRange = 10;
     [SerializeField]
     private float _damage;
 
-    //private void Start() 
-    //    => StartCoroutine(DestructionBullet());
+    private void Start()
+    {
+        _collider = GetComponent<Collider>();
+    }
 
     private void Update()
     {
         transform.position += _speed * Time.deltaTime * transform.forward;
         _distance = Vector3.Distance(transform.position, transform.parent.position);
 
-        if (_distance >= _firingRange) Destroy(gameObject);
+        if (_distance >= _minFiringRange) _collider.isTrigger = true;
+        if (_distance >= _maxFiringRange) Destroy(gameObject);
     }
-
-    //private IEnumerator DestructionBullet()
-    //{
-    //    yield return new WaitForSeconds(_firingRange);
-    //    Destroy(gameObject);
-    //}
 }
